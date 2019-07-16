28ef78251d92e5a290ef39117b5a2580beb59bfe
Andrey Tolstoy <avtolstoy@gmail.com>
2019-07-16 22:50:14 +0700

    Squashed commit of the following:
    commit 4e7298a0e73795f271a5a591e53dbd5a3bb35817
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Mon Nov 2 10:38:40 2015 -0800
    
        Initialize i in mbedtls_ssl_write_certificate
    
    commit eb857617d1ae8770aac6c290e66f7b79173b5237
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Mon Nov 2 09:28:44 2015 -0800
    
        Move declarations to satisfy gcc
    
    commit 7df832991407e98d2ed2adabf5b6c26784f5dd19
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Fri Oct 30 17:49:21 2015 -0700
    
        Run scripts/generate_errors.pl
    
    commit 118b7872968dc69e0592a180604de311fc2ca47e
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Thu Oct 29 17:01:51 2015 -0700
    
        Allow configuration of supported certificate types and check intersection on server
    
    commit ff9f03196c3ec2cf79cf86b9879f0c99cdb9df10
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Thu Oct 29 14:45:28 2015 -0700
    
        Add debug messages for client/server cert type extension writing
    
    commit b651f92bf09d4f2a7aaf137fd31b1f7272e04113
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Thu Oct 29 13:58:24 2015 -0700
    
        Refactor out mbedtls_ssl_write_x509_certificate function
    
    commit 8f79e1e7ce212acd23924f736126a5f3e42e5153
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Thu Oct 29 11:47:02 2015 -0700
    
        Add missing program to gitignore
    
    commit 75e88dd42b6f1e773031635769e792d291575a20
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Thu Oct 29 09:59:24 2015 -0700
    
        Add buffer bounds checking to client/server cert type extension write functions
    
    commit abb5a50d494b938c2cc79e1facfee870a28ae76f
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Thu Oct 29 09:44:01 2015 -0700
    
        Change to runtime checks for client/server
    
    commit c319544ba726631ed7f7bd34ddb829ce5a4fb023
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Thu Oct 29 09:28:20 2015 -0700
    
        Move pk_context variable declaration to remove warning if raw public key mode not enabled
    
    commit 8501b296bd31320ea59af37272fb32a2ef184c2b
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Thu Oct 29 09:27:55 2015 -0700
    
        Remove unnecessary variable usage
    
    commit eec221eb5910698d47b84be9c56ff4e092c9ce7e
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Thu Oct 29 09:27:35 2015 -0700
    
        Style fixes
    
    commit e56bca358477af4db6e1d49eb0952e0c62ab9d33
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Thu Oct 29 09:15:26 2015 -0700
    
        rename MBEDTLS_RAW_PUBLIC_KEY_SUPPORT to MBEDTLS_SSL_RAW_PUBLIC_KEY_SUPPORT
    
    commit 9192e5e06702855ac6b21af89bece31ad3f64f51
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Wed Oct 28 13:09:14 2015 -0700
    
        Fix client writing of client/server certificate type extensions
    
    commit 8680b6f10c6e1bed0477a46b79b34209b5a495f1
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Fri Oct 9 18:02:48 2015 -0700
    
        Add client raw public key support
    
    commit fbe4439fe386285564110b5e4303064331b51dd6
    Author: Bryce Kahle <bkahle@gmail.com>
    Date:   Fri Oct 9 16:59:29 2015 -0700
    
        Add incomplete raw public key support (RFC7250)

--- library/error.c
+++ library/error.c
@@ -525,6 +525,8 @@ void mbedtls_strerror( int ret, char *buf, size_t buflen )
             mbedtls_snprintf( buf, buflen, "SSL - Internal-only message signaling that a message arrived early" );
         if( use_ret == -(MBEDTLS_ERR_SSL_CRYPTO_IN_PROGRESS) )
             mbedtls_snprintf( buf, buflen, "SSL - A cryptographic operation is in progress. Try again later" );
+        if( use_ret == -(MBEDTLS_ERR_SSL_NO_CERTIFICATE_TYPE_CHOSEN) )
+            mbedtls_snprintf( buf, buflen, "SSL - The server has no certificate types in common with the client" );
 #endif /* MBEDTLS_SSL_TLS_C */
 
 #if defined(MBEDTLS_X509_USE_C) || defined(MBEDTLS_X509_CREATE_C)
